cd33f5e614d16abe26ccd99d879b304b7c24a402
matcop <matcop@gmail.com>
2021-01-26 13:03:43 -0400

    primer Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//import CustomLogging from './CustomLogging';
+const express = require('express')
+const app = express();
+
+const hbs = require('hbs');
+require ('./hbs/helpers.js');
+
+const port=process.env.PORT|| 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//express hbs
+hbs.registerPartials(__dirname + '/views/parciales/')
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    nombre: 'mARcelo',
+    
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    
+  })
+});
+
+app.listen(port, () => { console.warn(`escuchando peticiones en el puerto ${port}`); })
+
